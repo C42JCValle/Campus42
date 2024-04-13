e68f61b18c4a65f21c40d1b5db5de349436d1bbb
C42JCValle <c42jcvalle@gmail.com>
2024-04-14 00:43:00 +0200

    try21

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -1,35 +1,77 @@
-#include <stdio.h>
+    /* ************************************************************************** */
+    /*                                                                            */
+    /*                                                        :::      ::::::::   */
+    /*   ft_strlcat.c                                       :+:      :+:    :+:   */
+    /*                                                    +:+ +:+         +:+     */
+    /*   By: gacel <gacel@student.42.fr>                +#+  +:+       +#+        */
+    /*                                                +#+#+#+#+#+   +#+           */
+    /*   Created: 2024/04/14 00:07:37 by gacel             #+#    #+#             */
+    /*   Updated: 2024/04/14 00:29:55 by gacel            ###   ########.fr       */
+    /*                                                                            */
+    /* ************************************************************************** */
 
-size_t ft_strlcat(char *dest, const char *src, size_t size)
-{
-    size_t c = 0;
-    size_t d = 0;
-    size_t total_dest = 0;
-    size_t total_src = 0;
+    #include <stdio.h>
+    #include <string.h>
 
-  
-    while (dest[c] != '\0' && c < size -1)
-    { 
-        c++;
-    }
-    total_dest = c;    
-
-    while (c < size -1)
+    size_t ft_strlcat(char *dest, const char *src, size_t size) 
     {
-        
-        dest[c] = src[d];
-        
+        size_t dest_len = 0;
+        size_t total_len = 0;
+        size_t src_len = 0;
+        size_t i = 0;
 
-        if(dest[c] == '\0')
-        {
-            return (d);
-        }
-        c++;
-        d++;
+    // Calcular la longitud actual de dest
+    while (dest[dest_len] != '\0') 
+    { 
+        dest_len++;
     }
-    total_src = d;
 
-    dest[c] = '\0';
+    // Calcular la longitud de src
+    while (src[src_len] != '\0') 
+    {
+        src_len++;
+    }
 
-    return (d);
+    total_len = dest_len + src_len; // Longitud total después de la concatenación
+
+    // Si size es 0, devolver la longitud total sin modificar dest
+    if (size == 0) 
+    {
+        return total_len;
+    }
+
+    // Concatenar src en dest hasta que se alcance size - 1 o src llegue a su fin
+    while (src[i] != '\0' && dest_len + i < size - 1) 
+    {
+        dest[dest_len + i] = src[i];
+        i++;
+    }
+    dest[dest_len + i] = '\0'; // Asegurar que dest esté nul-terminado
+
+    // Si total_len es mayor o igual que size, devolver size - 1
+    if (total_len >= size) 
+    {
+        return size - 1;
+    }
+
+    // De lo contrario, devolver total_len
+    return total_len;
 }
+
+
+
+
+    #include <string.h>
+    #include <stddef.h>
+    #include "libft.h"
+
+    int main() 
+    {
+        const char src[] = "Hola";
+        char dest[10] = "Mundo";
+        size_t n = 3;
+
+        size_t copied = ft_strlcat(dest, src, n);
+        printf("dest: %s\n", dest);
+        return (0);
+    }
--- Pruebas/ft_strlcat.c
+++ Pruebas/ft_strlcat.c
@@ -1,48 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
-size_t my_strlcat(char *dest, const char *src, size_t size) {
-    size_t dest_len = 0;
-    size_t src_len = 0;
-
-    // Calcular la longitud de dest
-    while (dest_len < size && dest[dest_len] != '\0') {
-        dest_len++;
-    }
-
-    // Calcular la longitud de src
-    while (src[src_len] != '\0') {
-        src_len++;
-    }
-
-    // Calcular el espacio disponible en dest (incluyendo el carácter nulo)
-    size_t space_left = size - dest_len - 1;
-
-    // Si no hay espacio disponible, devolver la longitud de dest + la longitud de src
-    if (space_left == 0) {
-        return dest_len + src_len;
-    }
-
-    // Copiar la cadena src a dest, teniendo en cuenta el espacio disponible
-    size_t i = 0;
-    while (i < space_left && src[i] != '\0') {
-        dest[dest_len + i] = src[i];
-        i++;
-    }
-
-    // Añadir el carácter nulo al final de dest
-    dest[dest_len + i] = '\0';
-
-    // Devolver la longitud total de la cadena concatenada
-    return dest_len + src_len;
-}
 
 int main() {
     char dest[] = "Hola";
     const char *src = "Mundo";
-    size_t n = 10;
+    size_t n = 3;
 
     // Concatenar la cadena src a dest usando my_strlcat
-    size_t concatenated = my_strlcat(dest, src, sizeof(dest));
+    size_t concatenated = strlcat(dest, src, sizeof(dest));
 
     // Imprimir el resultado
     printf("dest: %s\n", dest);
