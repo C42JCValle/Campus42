2976257c2e6568483be07e2e706e16e30b612894
C42JCValle <c42jcvalle@gmail.com>
2024-04-14 23:19:51 +0200

    GoGoGo1.2

--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -6,11 +6,48 @@
 /*   By: gacel <gacel@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 22:42:47 by gacel             #+#    #+#             */
-/*   Updated: 2024/04/14 22:42:59 by gacel            ###   ########.fr       */
+/*   Updated: 2024/04/14 23:15:52 by gacel            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void *memchr(const void *s, int c, size_t n)
+#include <string.h>
+#include <stdio.h>
+
+void *ft_memchr(const void *s, int c, size_t n)
 {
+    const unsigned char *str = s;
+    unsigned char x = c;
+    size_t b;
+
+    b = 0;
+    while (b < n)
+    {
+       
+        if (str[b] == x)
+        {
+            return (void *) (str + b);
+        }
+         b++;
+    }
+    
+    return (NULL);
     
 }
+
+int main ()
+{
+    const unsigned char *cadena = "Juan Carlos";
+    char buscar = 'a';
+    size_t bits = 12;   
+    void  *result = ft_memchr(cadena, buscar, bits);
+    printf("%p",result);
+    return (0);
+    
+}
+
+    
+    
+    
+    
+    
+    
