12b82f25a891cf186122cd7d5ccdc9f31a6889ee
C42JCValle <c42@jcvalle@gmail.com>
2024-04-15 11:03:54 +0200

    sign jcvalle

--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gacel <gacel@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jvalle-d <jvalle-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/14 22:42:47 by gacel             #+#    #+#             */
-/*   Updated: 2024/04/14 23:15:52 by gacel            ###   ########.fr       */
+/*   Created: 2024/04/14 22:42:47 by jvalle-d              #+#    #+#             */
+/*   Updated: 2024/04/15 11:01:11 by jvalle-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,21 +33,3 @@ void *ft_memchr(const void *s, int c, size_t n)
     return (NULL);
     
 }
-
-int main ()
-{
-    const unsigned char *cadena = "Juan Carlos";
-    char buscar = 'a';
-    size_t bits = 12;   
-    void  *result = ft_memchr(cadena, buscar, bits);
-    printf("%p",result);
-    return (0);
-    
-}
-
-    
-    
-    
-    
-    
-    
---
+++ Main/ft_memchr.c
@@ -0,0 +1,18 @@
+
+int main ()
+{
+    const unsigned char *cadena = "Juan Carlos";
+    char buscar = 'a';
+    size_t bits = 12;   
+    void  *result = ft_memchr(cadena, buscar, bits);
+    printf("%p",result);
+    return (0);
+    
+}
+
+    
+    
+    
+    
+    
+    
