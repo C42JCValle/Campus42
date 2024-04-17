e5efb734992f6f42e1a343d7cbd1741c571fa464
C42JCValle <c42@jcvalle@gmail.com>
2024-04-17 13:38:29 +0200

    Arreglos

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: jvalle-d <jvalle-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 22:39:20 by jvalle-d          #+#    #+#             */
-/*   Updated: 2024/04/17 13:14:03 by jvalle-d         ###   ########.fr       */
+/*   Updated: 2024/04/17 13:37:51 by jvalle-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,12 @@ char	*ft_strchr(const char *s, int c)
 	}
 	return (NULL);
 }
-/*int main(void)
+/*
+int main(void)
 {
-    char *datos = ft_strchr("Juan", '3');
+    char *datos = ft_strchr("JuJan", 'J');
     
-    printf("%p\n", datos);
+    printf("%s\n", datos);
     return 0;
 }
 */
--- Libft/ft_strrchr.c
+++ Libft/ft_strrchr.c
@@ -5,23 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jvalle-d <jvalle-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/09 18:40:16 by jvalle-d          #+#    #+#             */
-/*   Updated: 2024/04/17 12:51:55 by jvalle-d         ###   ########.fr       */
+/*   Created: 2024/04/17 13:30:13 by jvalle-d          #+#    #+#             */
+/*   Updated: 2024/04/17 13:38:18 by jvalle-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
 	const char	*str;
 
-	while (*s != '\0')
-	{
-		s++;
-	}
 	str = s;
-	while (*str >= 0)
+	while (*str != '\0')
+	{
+		str++;
+	}
+	while (str >= s)
 	{
 		if (*str == c)
 		{
@@ -31,11 +31,12 @@ char	*ft_strrchr(const char *s, int c)
 	}
 	return (NULL);
 }
-/*int main(void)
+/*
+
+int main(void)
 {
-    char *datos = ft_strrchr("Juan", 'r');
-    
-    printf("%p\n", datos);
+    char *datos = ft_strrchr("JuJan", 'J');    
+    printf("%s\n", datos);
     return 0;
 }
 */
