25a188943fdd393f442b1fba93271b4ad4642d6f
c42jcvalle <c42jcvalle@gmail.com>
2024-05-26 11:48:54 +0200

    Hide

--- Training/Rank Exam 02/Level 3/hide.c
+++ Training/Rank Exam 02/Level 3/hide.c
@@ -1,32 +1,38 @@
 
-#include <stdio.h>
+#include <unistd.h>
 
-int    ft_hide (char *s1, char *s2)
+void    ft_hide (char *s1, char *s2)
 {
     int c;
     int i;
-    int x;
 
     c = 0;
     i = 0;
-    x = 0;
-    while (s2[c] != '\0')
-    {       
-        while (s1[c] != s2[i])
+    while (s2[i] != '\0')
+    {
+        if (s1[c] == s2[i])
         {
-            i++;
-            if (s1[c] == s2)                        
-        }                   
-       
-  
+            c++;
+        }
+        i++;
     }
+
+    if (s1[c] == '\0')
+        {
+            write (1,"1",1);
+            write (1, "\n",1);
+            //return (1);
+        }
+
+    write (1,"0",1);
+    write (1, "\n",1);
+    //return (0);
  
     
 }
 
 int main (int argc, char **argv)
 {
-   
-    printf("%d\n",ft_hide (argv[1], argv[2]));
+    ft_hide(argv[1], argv[2]);
     return 0;
 }
